e43f007287c2ae6bfa60e01fa7eee8ec4b992df1
anto <antokantony@yahoo.com>
2020-06-08 19:58:34 +0530

    Updates

---
+++ precache-manifest.60ff0665095634acfc5268a887bbfb13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5320daae84f9c2b76aeb330f743787c8",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "1663bd3eafb161f34206",
+    "url": "/demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c5d6d8f21e8582644447",
+    "url": "/demo/static/js/2.f43cb4ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demo/static/js/2.f43cb4ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1663bd3eafb161f34206",
+    "url": "/demo/static/js/main.91f87e50.chunk.js"
+  },
+  {
+    "revision": "7171bc5868196d3ab7ce",
+    "url": "/demo/static/js/runtime-main.99ec04b9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
